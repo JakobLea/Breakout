02e53fe24d013742a1904f79b29fb1487d1e2295
JakobLea <jakob.lea@outlook.com>
2023-10-03 23:07:07 +0200

    1

--- src/states/PaddleSelectState.lua
+++ src/states/PaddleSelectState.lua
@@ -1,17 +1,3 @@
---[[
-    GD50
-    Breakout Remake
-
-    -- StartState Class --
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    Represents the state the game is in when we've just started; should
-    simply display "Breakout" in large text, as well as a message to press
-    Enter to begin.
-]]
-
 PaddleSelectState = Class{__includes = BaseState}
 
 function PaddleSelectState:enter(params)
@@ -19,8 +5,6 @@ function PaddleSelectState:enter(params)
 end
 
 function PaddleSelectState:init()
-    -- the paddle we're highlighting; will be passed to the ServeState
-    -- when we press Enter
     self.currentPaddle = 1
 end
 
@@ -41,7 +25,6 @@ function PaddleSelectState:update(dt)
         end
     end
 
-    -- select paddle and move on to the serve state, passing in the selection
     if love.keyboard.wasPressed('return') or love.keyboard.wasPressed('enter') then
         gSounds['confirm']:play()
 
@@ -62,7 +45,6 @@ function PaddleSelectState:update(dt)
 end
 
 function PaddleSelectState:render()
-    -- instructions
     love.graphics.setFont(gFonts['medium'])
     love.graphics.printf("Select your paddle with left and right!", 0, VIRTUAL_HEIGHT / 4,
         VIRTUAL_WIDTH, 'center')
@@ -70,33 +52,25 @@ function PaddleSelectState:render()
     love.graphics.printf("(Press Enter to continue!)", 0, VIRTUAL_HEIGHT / 3,
         VIRTUAL_WIDTH, 'center')
         
-    -- left arrow; should render normally if we're higher than 1, else
-    -- in a shadowy form to let us know we're as far left as we can go
+
     if self.currentPaddle == 1 then
-        -- tint; give it a dark gray with half opacity
         love.graphics.setColor(40/255, 40/255, 40/255, 128/255)
     end
     
     love.graphics.draw(gTextures['arrows'], gFrames['arrows'][1], VIRTUAL_WIDTH / 4 - 24,
         VIRTUAL_HEIGHT - VIRTUAL_HEIGHT / 3)
    
-    -- reset drawing color to full white for proper rendering
     love.graphics.setColor(1, 1, 1, 1)
 
-    -- right arrow; should render normally if we're less than 4, else
-    -- in a shadowy form to let us know we're as far right as we can go
     if self.currentPaddle == 4 then
-        -- tint; give it a dark gray with half opacity
         love.graphics.setColor(40/255, 40/255, 40/255, 128/255)
     end
     
     love.graphics.draw(gTextures['arrows'], gFrames['arrows'][2], VIRTUAL_WIDTH - VIRTUAL_WIDTH / 4,
         VIRTUAL_HEIGHT - VIRTUAL_HEIGHT / 3)
     
-    -- reset drawing color to full white for proper rendering
     love.graphics.setColor(1, 1, 1, 1)
 
-    -- draw the paddle itself, based on which we have selected
     love.graphics.draw(gTextures['main'], gFrames['paddles'][2 + 4 * (self.currentPaddle - 1)],
         VIRTUAL_WIDTH / 2 - 32, VIRTUAL_HEIGHT - VIRTUAL_HEIGHT / 3)
 end
