12c437a30a05e153641351fc374be54ee517215e
JakobLea <jakob.lea@outlook.com>
2023-10-03 21:43:24 +0200

    ok

--- main.lua
+++ main.lua
@@ -24,6 +24,11 @@ function love.load()
         ['hearts'] = love.graphics.newImage('graphics/hearts.png'),
         ['particle'] = love.graphics.newImage('graphics/particle.png')
     }
+
+    gFrames = {
+        ['paddles'] = GenerateQuadsPaddles (gTextures['main']),
+        ['balls'] = GenerateQuadsBalls (gTextures['main'])
+    }
     
   
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -52,7 +57,8 @@ function love.load()
     }
 
     gStateMachine = StateMachine {
-        ['start'] = function() return StartState() end
+        ['start'] = function() return StartState() end,
+        ['play'] = function() return PlayState() end
     }
     gStateMachine:change('start')
 
--- src/Dependencies.lua
+++ src/Dependencies.lua
@@ -5,8 +5,12 @@ Class = require 'lib/class'
 
 require 'src/constants'
 
+require 'src/Paddle'
+
 require 'src/StateMachine'
 
+require 'src/Util'
 
 require 'src/states/BaseState'
+require 'src/states/PlayState'
 require 'src/states/StartState'
