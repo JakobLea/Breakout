aa99adea665a7e5d1d612bfa26a5da191dcce204
JakobLea <jakob.lea@outlook.com>
2023-10-03 23:05:40 +0200

    1

--- src/states/ServeState.lua
+++ src/states/ServeState.lua
@@ -1,23 +1,6 @@
---[[
-    GD50
-    Breakout Remake
-
-    -- ServeState Class --
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    The state in which we are waiting to serve the ball; here, we are
-    basically just moving the paddle left and right with the ball until we
-    press Enter, though everything in the actual game now should render in
-    preparation for the serve, including our current health and score, as
-    well as the level we're on.
-]]
-
 ServeState = Class{__includes = BaseState}
 
 function ServeState:enter(params)
-    -- grab game state from params
     self.paddle = params.paddle
     self.bricks = params.bricks
     self.health = params.health
@@ -26,19 +9,16 @@ function ServeState:enter(params)
     self.level = params.level
     self.recoverPoints = params.recoverPoints
 
-    -- init new ball (random color for fun)
     self.ball = Ball()
     self.ball.skin = math.random(7)
 end
 
 function ServeState:update(dt)
-    -- have the ball track the player
     self.paddle:update(dt)
     self.ball.x = self.paddle.x + (self.paddle.width / 2) - 4
     self.ball.y = self.paddle.y - 8
 
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
-        -- pass in all important state info to the PlayState
         gStateMachine:change('play', {
             paddle = self.paddle,
             bricks = self.bricks,
